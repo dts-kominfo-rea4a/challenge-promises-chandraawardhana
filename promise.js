1d6cf6d0c8721115a2876e5b7881ee3d9bc6dc78
chandraawardhana <chandraawardhana@gmail.com>
2022-09-18 02:15:38 +0700

    Tugas 02 - JS - Promises + Async & Await

--- promise.js
+++ promise.js
@@ -1,8 +1,15 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js')
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let promiseOutput = async value => {
+  const dataTheaterIXX = await promiseTheaterIXX()
+  const dataTheaterVGC = await promiseTheaterVGC()
+  var dataResult = dataTheaterIXX.concat(dataTheaterVGC)
+  let count = 0
+  dataResult.forEach(v => v.hasil === value && count++)
+  return count
+}
 
 module.exports = {
-  promiseOutput,
-};
+  promiseOutput
+}
